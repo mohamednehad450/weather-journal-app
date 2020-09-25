d3553cfcd777850dcd595e9a5bf27fdb726fc990
Mohamed Nehad <mohammednehad450@gmail.com>
2020-09-25 04:02:40 +0300

    feat: Implement getWeather

--- website/app.js
+++ website/app.js
@@ -6,24 +6,69 @@ const generate = document.querySelector('#generate');
 generate && generate.addEventListener('click', generateHandler);
 
 /* Function called by event listener */
+const baseURL = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
 function generateHandler(e) {
-    console.log('clicked')
-    getWeather()
-    .then(postData)
-    .then(getProjectData)
+    const zip = document.querySelector('#zip').value;
+    getWeather(baseURL, zip, apiKey)
+        .then(postEntry)
+        .then(getProjectData)
 }
 
 /* Function to GET Web API Data*/
-async function getWeather() {
-    console.log('Getting Weather');
+async function getWeather(baseURL, zip, apiKey) {
+
+    const url = `${baseURL}&zip=${zip}&appid=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const resutls = await response.json();
+            return resutls;
+        }
+        else {
+            throw new Error(`${response.status}: ${response.statusText}`);
+        }
+
+    } catch (error) {
+        // Fetch call faild
+        alert(error.message);
+    }
 }
 
 /* Function to POST data */
-async function postData() {
-    console.log('Posting Data');
+async function postEntry(weather) {
+    const { main: { temp: temperature } } = weather;
+    const userResponse = document.querySelector('#feelings').value;
+    const entry = { temperature, userResponse, date: new Date(), };
+    postData('/post', entry);
 }
 
 /* Function to GET Project Data */
 async function getProjectData() {
     console.log('Getting Project Data');
+}
+
+
+// Helper: posts data to a url
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        if (response.ok) {
+            const newData = await response.json();
+            return newData;
+        }
+        else {
+            throw new Error(`${response.status}: ${response.statusText}`);
+        }
+
+    } catch (error) {
+        alert(error.message);
+    }
 }
